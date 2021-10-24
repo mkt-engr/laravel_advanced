71ddab78211a77b048e54384537c60b0b735cbd1
Makito Mori <mkt.it.engr@gmail.com>
2021-10-24 23:04:11 +0900

    属性バック

--- umarche/resources/views/tests/component-test1.blade.php
+++ umarche/resources/views/tests/component-test1.blade.php
@@ -4,5 +4,7 @@
 
     <x-tests.card title="タイトル" content="本文" :message="$message" />
     <x-tests.card title="タイトル" />
-    <x-tests.card />
+
+    <x-tests.card title="CSSを変更したい" class="bg-red-300" />
+
 </x-tests.app>
